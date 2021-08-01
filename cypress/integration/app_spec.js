cc862d2ef9bf9f69c1ec2dc89a7328d31e193576
sabohat <sabohat2802@gmail.com>
2021-08-01 14:57:05 +0500

    fix: add timeout to cypres signup test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,28 +1,28 @@
-describe('Sign Up', () => {
-  it('Adds person to course', () => {
-    cy.visit('/')
+describe("Sign Up", () => {
+  it("Adds person to course", () => {
+    cy.visit("/");
 
     cy.get('input[name="name"]')
       .click()
-      .type('Some Name')
-      .should('have.value', 'Some Name')
+      .type("Some Name")
+      .should("have.value", "Some Name");
 
-    cy.get('input[name="email"]')
-      .click()
-      .type('some@email.com')
+    cy.get('input[name="email"]').click().type("some@email.com");
 
     cy.get('select[name="department"]')
-      .select('core')
-      .should('have.value', 'core')
+      .select("core")
+      .should("have.value", "core");
 
     cy.get('select[name="course"]')
-      .select('git-it')
-      .should('have.value', 'git-it')
+      .select("git-it")
+      .should("have.value", "git-it");
 
-    cy.get('input[type="submit"]')
-      .click()
+    cy.get('input[type="submit"]').click();
 
-    cy.get('li')
-      .should('contain', 'Some Name - some@email.com - core - git-it')
-  })
-})
+    // if the website is performing slowly, waits for 6 seconds to check
+    cy.get("li", { timeout: 8000 }).should(
+      "contain",
+      "Some Name - some@email.com - core - git-it"
+    );
+  });
+});
